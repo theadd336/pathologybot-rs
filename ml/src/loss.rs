f728476531f83134135e7a1e3e7276355e4384d0
Paul Addonizio <pauladdonizio@gmail.com>
2023-05-01 00:20:36 -0400

    Add softmax with specialization

--- ml/src/loss.rs
+++ ml/src/loss.rs
@@ -2,6 +2,12 @@ use ndarray::{ArcArray, Array, IxDyn};
 
 use crate::shared::MlNumber;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LossType {
+    CategoricalCrossEntropy,
+    Custom,
+}
+
 pub trait Loss<A: MlNumber> {
     fn compute_loss(
         &self,
@@ -14,4 +20,8 @@ pub trait Loss<A: MlNumber> {
         computed_output: ArcArray<A, IxDyn>,
         expected_output: ArcArray<A, IxDyn>,
     ) -> Array<A, IxDyn>;
+
+    fn loss_type(&self) -> LossType {
+        LossType::Custom
+    }
 }
